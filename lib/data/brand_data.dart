97f688472ab72a39c0dc7137d31bed052a247e63
AhmetRecaiElcan <ahmetrecaiellcan@gmail.com>
2025-01-09 09:19:02 +0300

    all brand data succesfull

--- lib/data/brand_data.dart
+++ lib/data/brand_data.dart
@@ -20,13 +20,13 @@ List<Brand> getBrandList() {
       name: "Ford",
       image: "assets/images/ford.png",
       productCount: 265,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Toyota",
       image: "assets/images/toyota.jpg",
       productCount: 95,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Fiat",
@@ -37,7 +37,7 @@ List<Brand> getBrandList() {
       name: "Tesla",
       image: "assets/images/tesla.jpg",
       productCount: 36,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Chery",
@@ -48,171 +48,200 @@ List<Brand> getBrandList() {
       name: "Audi",
       image: "assets/images/audi.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Dacia",
       image: "assets/images/dacia.jpg",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Hyundai",
       image: "assets/images/hyundai.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Kia",
       image: "assets/images/kia.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Opel",
       image: "assets/images/opel.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Togg",
       image: "assets/images/togg.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Toyota",
       image: "assets/images/toyota.jpg",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Volkswagen",
       image: "assets/images/volkswagen.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Renault",
       image: "assets/images/renault.jpg",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Mercedes",
       image: "assets/images/mercedes.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Volvo",
       image: "assets/images/volvo.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Nissan",
       image: "assets/images/nissan.jpg",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
       name: "Land Rover",
       image: "assets/images/landrover.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Byd",
+      image: "assets/images/byd.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Honda",
+      image: "assets/images/honda.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Bmw",
+      image: "assets/images/bmw.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Alfa Romeo",
+      image: "assets/images/alfaromeo.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Cupra",
+      image: "assets/images/cupra.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Citroen",
+      image: "assets/images/citroen.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Dfsk",
+      image: "assets/images/dfsk.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Ds",
+      image: "assets/images/ds.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Jaecco",
+      image: "assets/images/jaecco.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Jeep",
+      image: "assets/images/jeep.jpg",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Lexus",
+      image: "assets/images/lexus.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Mg",
+      image: "assets/images/mg.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Mini",
+      image: "assets/images/mini.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Mitsubishi",
+      image: "assets/images/mitsubishi.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
     Brand(
-      name: "Audi",
-      image: "assets/images/audi.png",
+      name: "Peugeot",
+      image: "assets/images/peugeot.png",
       productCount: 88,
-      isVerified: true
+      isVerified: false
     ),
-    
+    Brand(
+      name: "Seat",
+      image: "assets/images/seat.png",
+      productCount: 88,
+      isVerified: false
+    ),
+    Brand(
+      name: "Skoda",
+      image: "assets/images/skoda.png",
+      productCount: 88,
+      isVerified: false
+    ),
+    Brand(
+      name: "Ssangyong",
+      image: "assets/images/ssangyong.png",
+      productCount: 88,
+      isVerified: false
+    ),
+    Brand(
+      name: "Subaru",
+      image: "assets/images/subaru.jpg",
+      productCount: 88,
+      isVerified: false
+    ),
+    Brand(
+      name: "Suzuki",
+      image: "assets/images/suzuki.png",
+      productCount: 88,
+      isVerified: false
+    ),
 
     // Diğer markalar buraya eklenebilir...
   ];
