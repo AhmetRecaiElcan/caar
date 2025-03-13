749dcd24ad927d5a1ada634200baf3d4c0925bec
AhmetRecaiElcan <ahmetrecaiellcan@gmail.com>
2025-03-13 14:40:31 +0300

    images kısmı komple değişti resimler kaldırıldı artık datadan çekiliyor

--- lib/data/car_data.dart
+++ lib/data/car_data.dart
@@ -10,209 +10,206 @@ List<Car> getCarList(){
       "Weekly",
       [
         "https://drive.google.com/uc?id=1IGiDqyrsjVBMQbatwcF5UeCgGxUQRH13",
-        "https://drive.google.com/uc?id=1IGiDqyrsjVBMQbatwcF5UeCgGxUQRH13",
-        "https://drive.google.com/uc?id=1IGiDqyrsjVBMQbatwcF5UeCgGxUQRH13",
+        "https://drive.google.com/uc?id=1vPaPHUYW_1iMdJF3UcCnfldBAusP8Mlr",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Alfa Romeo",
       "C4",
       "3580",
       "Monthly",
       [
-        "assets/images/alfa_romeo_c4_0.png",
+        "https://drive.google.com/uc?id=16-tgbm02XfWUN2mR6BklOEkmz0W_pGfh",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Byd",
       "Dolphin",
       "1.284.500",
       "Monthly",
       [
-        "assets/images/byd/byd_1.png",
-        "assets/images/byd/byd_0.png",
-         "assets/embed/byd/b.glb",
+        "https://drive.google.com/uc?id=1iQ2UDcTWKIEhi3QaBk8kQiyY3GoiMwZW",
+        "https://drive.google.com/uc?id=1QCdVo5G_vj4Vwq2S3I86DtezSaDi_Kp5",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Byd",
       "Atto 3",
       "1494500",
       "Monthly",
       [
-        "assets/images/byd/byd_2.png",
-        "assets/images/byd/byd_3.png",
-        "assets/embed/byd/bydatto.glb",
+        "https://drive.google.com/uc?id=1vYODqW8jOirqeX_yiK-okCV0c61DSnTf",
+        "https://drive.google.com/uc?id=1rZqsXQf5oEaxO0Om0236fSyxOxT1Lv7L",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Byd",
       "Seal U Ev",
       "1797000",
       "Weekly",
       [
-        "assets/images/byd/byd_4.png",
-        "assets/images/byd/byd_5.png",
+        "https://drive.google.com/uc?id=11ZBym-sEneO5BY3cw_O733OugSAT1O5T",
+        "https://drive.google.com/uc?id=1ZkQuXv16h6t42RkSQHnsW9SslJSH9mrU",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Byd",
       "Byd Seal U DM-İ",
       "1857750",
       "Weekly",
       [
-        "assets/images/byd/byd_6.png",
-        "assets/images/byd/byd_7.png",
-        "assets/images/byd/byd_8.png",
+        "https://drive.google.com/uc?id=1bLJB9xmnlPxtdVxHm48QteV4Dz1bis9y",
+        "https://drive.google.com/uc?id=1_2lOdKbxGWwYF5VBAiggmc023V7FqVXE",
+        "https://drive.google.com/uc?id=1efSH40vf5KqGY-Qm_zcpM_avaTQL6976",
       ],
-    ), Car(
+        ), Car(
       "Byd",
       "Byd Seal U AWD",
       "2453250",
       "Weekly",
       [
-        "assets/images/byd/byd_10.png",
-        "assets/images/byd/byd_11.png",
+        "https://drive.google.com/uc?id=10i20_2Jh4fV9azhu2tr0bfQJ8kozJMJY",
+        "https://drive.google.com/uc?id=1rCDXmIf1E2t2aHebygk18D37RagVZxfu",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Byd",
       "Byd Seal Han",
       "3213750",
       "Weekly",
       [
-        "assets/images/byd/byd_12.png",
-        "assets/images/byd/byd_13.png",
-        "assets/images/byd/byd_14.png",
+        "https://drive.google.com/uc?id=11dSsIYS573MAW-6V_E5DUv6tMs1CdSF1",
+        "https://drive.google.com/uc?id=1lmLhSbQJhYzZx_NAjV24jC1wc74hJzJO",
+        "https://drive.google.com/uc?id=1LrBJADmTijj-kLT9eEPsbzELisWO0Cqd",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Tesla",
       "Model S",
       "4350",
       "Weekly",
       [
-        "assets/images/tesla_model_s_0.png",
+        "https://drive.google.com/uc?id=1vUZuHdNizaT0vEPw3dfIKI0QHTdVmBui",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Honda",
       "CR-V E:HEV",
       "4151000",
       "3+3 Yıl Garanti",
       [
-        "assets/images/honda/honda_1.png",
-        "assets/images/honda/honda_2.png",
-        "assets/images/honda/honda_3.png",
+        "https://drive.google.com/uc?id=1F6PDxtSz9HW_uRianOWFiYw4ReJ9WeV7",
+        "https://drive.google.com/uc?id=13XPT4u3G2in0JH5ar4vv0OlXonpM-cEr",
+        "https://drive.google.com/uc?id=1tvuCMYX1hDrauMvfbAlgkT0QyAk0XDcL",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Honda",
        "ZR-V E:HEV",
       "3138000",
       "3+3 Yıl Garanti",
       [
-        "assets/images/honda/honda_4.png",
-        "assets/images/honda/honda_5.png",
+        "https://drive.google.com/uc?id=1kz_dYyUpHi0WeYXydFXn8cnb4UW2zf6M",
+        "https://drive.google.com/uc?id=15StqdezaaoyLAF6Nxjejil8tsdNaD5Rh",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Honda",
        "HR-V E:HEV",
       "1928000",
       "3+3 Yıl Garanti",
       [
-        "assets/images/honda/honda_hrv0.png",
-        "assets/images/honda/honda_hrv1.png",
-        "assets/images/honda/honda_hrv2.png",
+        "https://drive.google.com/uc?id=1wFNG9k9MxJoa3Rkdi2pQGL6CaWoTn3Yz",
+        "https://drive.google.com/uc?id=1g5hnoGpxwzWpFrSqQp-2FQeYOLM-NI4R",
+        "https://drive.google.com/uc?id=1QSqIUj5x_iGjfYAj9qBsJVpn0mjUH8ux",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Honda",
        "JAZZ E:HEV",
       "1413000" ,
       "3+3 Yıl Garanti ",
       [
-        "assets/images/honda/honda_jazz0.png",
-        "assets/images/honda/honda_jazz1.png",
-        "assets/images/honda/honda_jazz2.png",
+        "https://drive.google.com/uc?id=1Ayo0UlGKv8C29D41FQCu3D35RnX_ASn0",
+        "https://drive.google.com/uc?id=1INKDXwJI8ieXByoFPxpPRcUVun1qtcA4",
+        "https://drive.google.com/uc?id=1g8Po64NVW7ns9ax5SijVaYftydDqJ6UF",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Honda",
-       "Civic",
-      "2040000",
-      "3+3 Yıl Garanti",
-      [
-        "assets/images/honda/honda_civic0.png",
-        "assets/images/honda/honda_civic1.png",
-        "assets/images/honda/honda_civic2.png",
-      ],
-    ),
-    Car(
-      "Honda",
-       "City",
-      "1335000",
-      "3+3 Yıl Garanti",
-      [
-        "assets/images/honda/honda_city0.png",
-        "assets/images/honda/honda_city1.png",
-        "assets/images/honda/honda_city2.png",
-      ],
-    ),
-    Car(
+        "Civic",
+            "2040000",
+            "3+3 Yıl Garanti",
+            [
+         "https://drive.google.com/uc?id=1HX-ITua1kc1I8rTBFCntEMQwI0_Rewy8",
+         "https://drive.google.com/uc?id=1E63eVHr_Aw4MaL6JaJP4pMeneFCQJmc6",
+         "https://drive.google.com/uc?id=12Jx9V1AD4u6V5C0RZHCwn45OK5h80fel",
+            ],
+         ),
+         Car(
+            "Honda",
+        "City",
+            "1335000",
+            "3+3 Yıl Garanti",
+            [
+         "https://drive.google.com/uc?id=1O1-Ab95YWKz-Lh-CsiTnDJRtlCDy4XBd",
+         "https://drive.google.com/uc?id=1GwGXrlO4UwAjSMDoVzdnnJHoke8wY5bs",
+         "https://drive.google.com/uc?id=1grWYh0WaNFcB4uGmy9NfCL64lgCmJ_CB",
+            ],  ),
+        Car(
       "Honda",
       "Type R",
       "4.261.000",
       "3+3 Yıl Garanti",
       [
-        "assets/images/honda/honda_typer0.png",
-        "assets/images/honda/honda_typer1.png",
-        "assets/images/honda/honda_typer2.png",
+        "https://drive.google.com/uc?id=15WYgciNl7qO7vuwa7pcPX-vPYhkOv_nk",
+        "https://drive.google.com/uc?id=1fA_hhJry05zZrZhTwF27PsRLkRAyFuCZ",
+        "https://drive.google.com/uc?id=1VBdRtXylRhCk6eWPPhMLZEAu1eUd7WF9",
+        "https://drive.google.com/uc?id=1NZSaLO7kdPOxy9YI0BjvU6YptkWnN1t-",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Fiat",
       "Egea Cross",
       "1.000.000",
       "3 yıl (60.000 km) garanti",
       [
-        "assets/images/fiat/fiat_egea1.png",
-        "assets/images/fiat/fiat_egea0.png",
-        "assets/images/fiat/fiat_egea2.png",
+        "https://drive.google.com/uc?id=1WzpuYSXUMIGZKSq8Xql5SoCprGl5VjxE",
+        "https://drive.google.com/uc?id=13sbQmsowuC4rzKQt-9bC8ESreGuVfwkH",
+        "https://drive.google.com/uc?id=1wDaNw54iNBqP1i-UNoasQS6OOfi_K5C_",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Fiat",
       "Egea Sedan",
       "1.084.900 ",
       "3 yıl (60.000 km) garanti",
       [
-        "assets/images/fiat/fiat_egea2.png",
-        "assets/images/fiat/fiat_egea3.png",
+        "https://drive.google.com/uc?id=1wDaNw54iNBqP1i-UNoasQS6OOfi_K5C_",
+        "https://drive.google.com/uc?id=1KdvW0iX3sUTkht6f5-VlgpsMIVK5Z4J7",
       ],
-    ),
-    Car(
+        ),
+        Car(
       "Fiat",
       "Topolino",
       "475.900",
       "3. Yıl ya da 150.000 Km",
       [
-        "assets/images/fiat/fiat_topolino0.jpg",
-        "assets/images/fiat/fiat_topolino1.jpg",
-        "assets/images/fiat/fiat_topolino2.jpg",
+        "https://drive.google.com/uc?id=1L1ClMzyBRI7RSZhBfIhCUdXSjCxEDkYy",
+        "https://drive.google.com/uc?id=1VJIxY-KP6ycSKy0PFgCVTctKIEtN4UaI",
+        "https://drive.google.com/uc?id=1aYWaAYqDBiib5P4vMsnzNcu7JuOnLqUQ",
       ],
-    ),
-    Car("Fiat",
+        ),
+        Car("Fiat",
       "600",
       "1.415.900",
       "3 yıl (60.000 km) garanti",
       [
-        "assets/images/fiat/fiat_6000.png",
-        "assets/images/fiat/fiat_6001.png",
-        "assets/images/fiat/fiat_6002.png",
+        "https://drive.google.com/uc?id=1_1E8Rgg5_OT1mEfUFVHAil3WAbu5BAYV",
+        "https://drive.google.com/uc?id=1HJb-Vv52aHkp3pRvkr-TgiEj7ANTCq06",
+        "https://drive.google.com/uc?id=1IGiDqyrsjVBMQbatwcF5UeCgGxUQRH13",
       ],
-    ),
+        ),
     // ... diğer arabaların listesi buraya taşınacak ...
   ];
 } 
